f129c0c5aa6b52e57279ed53f449eb7df776db2c
Ahmad Mozaffar <aksoftware98@hotmail.com>
2018-10-20 21:43:42 +0300

    Add project files.

---
+++ Homework1/Homework1/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Homework1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Set the images from resources 
+            coverImage.Source = ImageSource.FromResource("Homework1.Images.hero.jpg");
+            profileImage.Source = ImageSource.FromResource("Homework1.Images.employee.jpg"); 
+        }
+    }
+}
---
+++ Homework1/Homework1/SettingsWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Homework1
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SettingsWindow : ContentPage
+	{
+		public SettingsWindow ()
+		{
+			InitializeComponent ();
+		}
+
+        private async void btnSave_Clicked(object sender, EventArgs e)
+        {
+            await btnSave.TranslateTo(0, 10, 100);
+            await btnSave.TranslateTo(0, 0, 100, Easing.SinOut);
+
+            await DisplayAlert("Alert", "Data has been Saved", "Okay");
+        }
+    }
+}
